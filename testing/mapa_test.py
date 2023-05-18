75abb3039540a42263d8f5987b1ad7f23783b538
Luis Alvarado <99617682+1u1s4@users.noreply.github.com>
2023-05-18 11:45:07 -0500

    v 0.6.2
    texto centrado

--- testing/mapa_test.py
+++ testing/mapa_test.py
@@ -1,4 +1,5 @@
 from colorimapgt import mapa
+import random
 import pandas as pd
 
 # Crear una instancia de la clase Mapa
@@ -6,7 +7,7 @@ m = mapa.Mapa(
     nombre_archivo='miMapa',
     datos=pd.DataFrame({
           'region': ['Región I', 'Región II', 'Región III', 'Región IV', 'Región V', 'Región VI', 'Región VII', 'Región VIII'],
-          'valores': [1, 2, 3, 4, 5, 6, 7, 8]
+          'valores': random.sample(range(1, 100), 8)
           }),
     color_base='#EB5BB1'
     )
