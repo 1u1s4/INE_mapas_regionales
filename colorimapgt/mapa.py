f419c34771cb112b14540fffdaa99999cd46e257
Luis Alvarado <99617682+1u1s4@users.noreply.github.com>
2023-05-18 12:21:08 -0500

    v 0.6.4

--- colorimapgt/mapa.py
+++ colorimapgt/mapa.py
@@ -35,17 +35,18 @@ class Mapa:
         color_hex = color_hex.lstrip("#")
         color_base = tuple(int(color_hex[i:i+2], 16) / 255.0 for i in range(0, 6, 2))
 
+        # Crea el mapa de colores personalizado que va desde blanco hasta el color base y desde el color base hasta negro
         color_scale = mcolors.LinearSegmentedColormap.from_list(
-            "scale", [(1.0, 1.0, 1.0), color_base]
+            "scale", [(1.0, 1.0, 1.0), color_base, (0.0, 0.0, 0.0)]
         )
 
         # Genera una lista de valores entre 0 y 1
-        scale_values = np.linspace(0, 1, 100)
+        scale_values = np.linspace(0, 1, 200) # Ajustado para tener en cuenta el color base adicional
 
         # Utiliza los valores de la escala para obtener los colores correspondientes de la escala de colores
         colors = [mcolors.rgb2hex(color_scale(val)) for val in scale_values]
 
-        return colors[50::3]
+        return colors[50:100] # Esto también puede necesitar ser ajustado dependiendo de cómo quieras que se vea la escala de colores
 
     def agregar_datos(self, datos_nuevos: list) -> None:
         self.datos = pd.DataFrame({
@@ -98,3 +99,21 @@ class Mapa:
 
         # Vuelve al directorio de trabajo original
         os.chdir(original_dir)
+
+import random
+import pandas as pd
+
+# Crear una instancia de la clase Mapa
+m = Mapa(
+    nombre_archivo='miMapa',
+    datos=pd.DataFrame({
+          'region': ['Región I', 'Región II', 'Región III', 'Región IV', 'Región V', 'Región VI', 'Región VII', 'Región VIII'],
+          'valores': random.sample(range(1, 100), 8)
+          })
+    )
+
+# Hacer el mapa
+m.hacer_mapa()
+
+# Compilar el archivo LaTeX
+m.compilar()
