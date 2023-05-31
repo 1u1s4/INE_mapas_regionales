e65fbd324974e201598385c8e43f2a0475483739
Luis Alvarado <99617682+1u1s4@users.noreply.github.com>
2023-05-31 10:47:09 -0600

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,13 +2,13 @@ from setuptools import setup, find_packages
 
 setup(
     name='colorimapgt',
-    version='0.6.4',
+    version='0.6.5',
     author='Luis Alfredo Alvarado Rodríguez',
     description='Mapas colorimetricos a nivel regional de Guatemala.',
     long_description='',
     url='https://github.com/1u1s4/INE_mapas_regionales',
     keywords='development, setup, setuptools',
-    python_requires='>=3.9',
+    python_requires='>=3',
     packages=find_packages(),
     py_modules=['mapa'],
     install_requires=[
