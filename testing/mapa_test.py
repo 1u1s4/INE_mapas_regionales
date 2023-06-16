84af43ed2cbfa4f38d2636b6bfad6eab1cf0d8be
Luis Alvarado <99617682+1u1s4@users.noreply.github.com>
2023-06-16 11:24:48 -0600

    Update mapa_test.py

--- testing/mapa_test.py
+++ testing/mapa_test.py
@@ -7,10 +7,10 @@ m = mapa.Mapa(
     nombre_archivo='miMapa',
     datos=pd.DataFrame({
           'region': ['Región I', 'Región II', 'Región III', 'Región IV', 'Región V', 'Región VI', 'Región VII', 'Región VIII'],
-          'valores': random.sample(range(1, 100), 8)
+          'valor': random.sample(range(1, 100), 8)
           }),
     color_base='#EB5BB1',
-    decimales=0
+    precision=0
     )
 
 # Hacer el mapa
