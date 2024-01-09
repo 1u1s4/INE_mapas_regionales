82e173d829a3908303935152a90bdb4233e25fdd
Rodrigo Castillo <steverogersavengers@gmail.com>
2024-01-09 10:49:07 -0600

    versión con barra modificada

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='colorimapgt',
-    version='0.7.8',
+    version='0.7.9',
     author='Luis Alfredo Alvarado Rodríguez',
     description='Mapas colorimetricos a nivel regional de Guatemala.',
     long_description='',
